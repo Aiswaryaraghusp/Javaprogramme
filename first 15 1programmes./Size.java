86b122503f14c6f1a56c89ea18cf528d0dcc4a41
Aiswaryaraghusp <79993774+Aiswaryaraghusp@users.noreply.github.com>
2021-05-26 20:49:28 +0530

    commit size

---
+++ first 15 1programmes./Size.java
@@ -0,0 +1,21 @@
+public class Size{
+public static void main(String[] args){
+int number=29;
+switch(number)
+{
+ case 29:System.out.println("small");
+         break;
+
+ case 42:System.out.println("Medium");
+         break;
+case 44:System.out.println("large");
+         break;
+case 48:System.out.println("Extralarge");
+         break;
+ default:System.out.println("unknown");
+}
+}
+}
+
+
+
