11c94305e08c38cb8b813dc578fcbacf60586c6b
Aiswaryaraghusp <79993774+Aiswaryaraghusp@users.noreply.github.com>
2021-05-26 17:27:01 +0530

    commit largest among three

---
+++ first 15 1programmes./Largest.java
@@ -0,0 +1,21 @@
+public class Largest{
+  public static void main(String[] args){
+ int x=6;
+ int y=5;
+ int z=3;
+int l=0;
+ if(x>y)
+{
+  l=x;
+}
+else
+{
+ l=y;
+}
+if(l<z)
+{
+   l=z;
+}
+System.out.println(l);
+}
+}
