47dcfba8e921a3a987c51655ff4700e4a844d071
Aiswaryaraghusp <79993774+Aiswaryaraghusp@users.noreply.github.com>
2021-05-26 20:48:18 +0530

    commit array foreachloop

---
+++ first 15 1programmes./Arrayloop.java
@@ -0,0 +1,10 @@
+public class Arrayloop{
+public static void main(String[] args){
+
+int[] a={1,2,3,4,5};
+System.out.println("The array elements are");
+for(int i:a)
+
+System.out.println(i);
+}
+}
