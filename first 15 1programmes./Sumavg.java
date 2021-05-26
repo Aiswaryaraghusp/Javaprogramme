c181c60c8d400f7110702892ae7395be419f1918
Aiswaryaraghusp <79993774+Aiswaryaraghusp@users.noreply.github.com>
2021-05-26 13:01:46 +0530

    commit sumavg

---
+++ first 15 1programmes./Sumavg.java
@@ -0,0 +1,15 @@
+public class Sumavg{
+  public static void main(String[] args){
+ int a=1;
+  int b=2;
+
+int c=3;
+  int d=4;
+int e=5;
+  int sum=a+b+c+d+e;
+   System.out.println(sum);
+  
+   System.out.println((sum)/5);
+  
+}
+}
